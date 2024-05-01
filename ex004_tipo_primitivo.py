988baa60ae307c34e3bc5b6467af36b77ef9dd6c
Alessandro Guedes <122487562+alessandroguedes@users.noreply.github.com>
2024-05-01 16:40:38 -0300

    first commmit

---
+++ ex004_tipo_primitivo.py
@@ -0,0 +1,10 @@
+
+a = input("Digite algo: ")
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um numero inteiro? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em Maiúscula? ', a.isupper())
+print('Está em Minúscula? ', a.islower())
+print('Está capitalizada? ', a.istitle())
