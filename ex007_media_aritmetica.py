a8f1f6959675fdc7845e4659be9b49d43bce7e21
Alessandro Guedes <122487562+alessandroguedes@users.noreply.github.com>
2024-05-09 17:27:05 -0300

    atualizar exercicios

--- ex007_media_aritmetica.py
+++ ex007_media_aritmetica.py
@@ -3,4 +3,4 @@ n2 = float(input('Digite outro númmero: '))
 
 media = (n1 + n2)/2
 
-print('A Média aritmética entre {} e {} é {}'.format(n1, n2, media))
+print('A Média aritmética entre {} e {} é {:.2f}'.format(n1, n2, media))
---
+++ ex008_conversor_medidas.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número em metros: '))
+km = n*1000
+hm = n*100
+dam = n*10
+dm = n/10
+cm = n/100
+mm = n/1000
+print('A medida de {}m corresponde a \n{}km \n{}hm \n{}dam \n{}dm \n{}cm \n{}mm'.format(n, km, hm, dam, dm, cm, mm))
